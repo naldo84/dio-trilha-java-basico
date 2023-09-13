0994661c7d15957cc0afb08a70acc479de4572e9
Erinaldo <erinaldo2005@gmail.com>
2023-09-12 22:46:50 -0300

    Condição Ternária

--- Aulas/controle-fluxo/src/ResultadoEscolar.java
+++ Aulas/controle-fluxo/src/ResultadoEscolar.java
@@ -15,6 +15,16 @@ public class ResultadoEscolar {
         } else {
             System.out.println("Reprovado");
         }
+
+        //UTILIZANDO O OPERADOR TERNÁRIO
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println("Resultado (C/ operador Ternário): " + resultado);
+
+        //UTILIZANDO O OPERADOR TERNÁRIO COM IF ENCADEADO
+        String resultado2 = nota >= 7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println("Resultado do if encadeado utilizando o operador Ternário: " + resultado2);
         
     }
     
