ab605cad13c22b0c3289589e49c74fd33654fcd1
Erinaldo <erinaldo2005@gmail.com>
2023-09-12 22:26:01 -0300

    Estrutua Condicional Simple e Composta

---
+++ Aulas/controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite uma nota: ");
+        double nota = scan.nextDouble();
+
+        if (nota >= 7)
+            System.out.println("Aprovado!!");
+        else 
+            System.out.println("Reprovado");
+        
+    }
+    
+}
