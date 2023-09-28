7388c1d9eeddd3165e2e166f40917709a64e528d
Erinaldo <erinaldo2005@gmail.com>
2023-09-27 22:28:30 -0300

    POO - List

---
+++ Aulas/POO/Collections/List/collections-java-api-2023/src/operacoesBasicas/ListaTarefas.java
@@ -0,0 +1,20 @@
+package operacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+
+    }
+}
